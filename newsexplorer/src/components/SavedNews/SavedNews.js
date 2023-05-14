5f623a069a146d53566a98ba2a09b4b2171a44ea
romans1995 <cboby26@gmail.com>
2023-05-14 13:43:40 +0300

    add sections to savedarticles

--- newsexplorer/src/components/SavedNews/SavedNews.js
+++ newsexplorer/src/components/SavedNews/SavedNews.js
@@ -26,14 +26,14 @@ const SavedNews = () => {
     };
     return (
         <main className="savedNews">
-            <div className='savedNews__text'>
+            <section className='savedNews__text'>
                 <p className="savedNews__P">saved articles</p>
                 <h2 className='savedNews__hsecond'>{user.firstName},you have {data.length} saved articles</h2>
                 <p className='savedNews__keywords'>By keywords: <strong>{keywordSelect()}</strong></p>
-            </div>
-            <div className='NewsCardList-container'>
+            </section>
+            <section className='NewsCardList-container'>
                 <NewsCardList />
-            </div>
+            </section>
         </main>
     );
 }
