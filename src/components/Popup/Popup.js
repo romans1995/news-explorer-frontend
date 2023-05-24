22e550b1996fffcf8f8274af6766905bc9265d8b
romans1995 <cboby26@gmail.com>
2023-05-24 10:26:49 +0300

    improve Popup

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React,{ useEffect } from "react";
 import './Popup.css';
 const Popup = ({ isOpen, onClose, name, title, children, errMessage }) => {
+    useEffect(() => {
+        // with this we prevent adding the listener if the popup is not opened
+        if (!isOpen) return;
+        // we should define the handler inside `useEffect`, so that it wouldn’t lose the reference to be able to remove it
+        const closeByEscape = (e) => {
+            if (e.key === 'Escape') {
+                onClose();
+            }
+        }
+
+        document.addEventListener('keydown', closeByEscape)
+   
+        return () => document.removeEventListener('keydown', closeByEscape)
+        // here we watch `isOpen` to add the listener only when it’s opened
+    }, [isOpen, onClose])
+
+    // here is the overlay handler
+    const handleOverlay = (e) => {
+        if (e.target === e.currentTarget) {
+            onClose();
+        }
+    }
+
  return (
         <div
             className={`popup ${name} ${isOpen ? "popup__active" : ""}`}
+         onClick={handleOverlay}
         >
             <div className="popup__container">
                 <button
